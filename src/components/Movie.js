c62e9176193826b85a35e67e0573cc8b2fb05b27
HS-Jeong88/ <wjdgksto88@gmail.com>
2021-09-28 16:21:47 +0900

    second commit

---
+++ src/components/Movie.js
@@ -0,0 +1,39 @@
+/* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
+import { useMutation } from "@apollo/client";
+import gql from "graphql-tag";
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const LIKE_MOVIE = gql`
+  mutation toggleLikeMovie($id: Int!) {
+    toggleLikeMovie(id: $id, isLiked: $isLiked) @client
+  }
+`;
+
+const Container = styled.div`
+  height: 380px;
+  width: 100%;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  border-radius: 7px;
+`;
+
+const Poster = styled.div`
+  background-image: url(${(props) => props.bg});
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+`;
+
+export default ({ id, bg, isLiked }) => {
+  const [toggleLikeMovie] = useMutation(LIKE_MOVIE, { variables: { id: parseInt(id), isLiked } });
+  return (
+    <Container>
+      <Link to={`/${id}`}>
+        <Poster bg={bg} />
+      </Link>
+      <button onClick={toggleLikeMovie}>{isLiked ? "Unlike" : "Like"}</button>
+    </Container>
+  );
+};
